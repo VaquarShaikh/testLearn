e69d83dd7525e1cbcc61d1307409e9b3ad42970e
root <root@LAPTOP-V7PLLBH9.>
2024-07-23 06:34:53 +0000

    Merge Conflict check .

--- Test.cpp
+++ Test.cpp
@@ -1,7 +1,31 @@
-#include<iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
-using namespace  std;
+using namespace std;
 
-int main(){
-  cout<<"Hello"<<endl;
+typedef long long ll;
+typedef pair<int, int> pii;
+#define ln "\n"
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define INF 2e18
+#define INFF 1000000009
+
+int main() {
+  // Hello World
+  cout << "Hello world !" << endl;
 }
